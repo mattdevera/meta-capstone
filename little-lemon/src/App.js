181940b05d809e4f6e712fcd1dc936d5aa832f34
Matthew De Vera <deveramatthew1@gmail.com>
2023-07-08 13:03:09 -0700

    Implemented semantics for the website

--- little-lemon/src/App.js
+++ little-lemon/src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header.js';
+import Main from './Main.js';
+import Footer from './Footer.js';
 
 function App() {
   return (
-    <div className="App">
-      Homepage
-    </div>
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
   );
 }
 
